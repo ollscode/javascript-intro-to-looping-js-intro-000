f40e0d4bf0994ed5aef40781bc4569acde446e04
Oscar <mundonatural@gmail.com>
2020-02-10 14:16:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 
 function forLoop(array) {
   for (var i = 0; i < array.length; i++) {
-    if (i == 11) {
-      array[i] = 'I am 1 strange loop.';
+    if (i === 1) {
+      array[i] = `I am 1 strange loop.`;
     } else {
       array[i] = `I am ${i} strange loops.`;
     }
